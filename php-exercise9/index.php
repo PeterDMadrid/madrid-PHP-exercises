2d6c698c6b2288c16f775d9bb9d53c1ece148d44
Peter Madrid <petermadrid0421@gmail.com>
2024-03-14 13:54:50 +0800

    mvc

---
+++ php-exercise9/index.php
@@ -0,0 +1,2 @@
+<?php
+include "src/control/control.php";
---
+++ php-exercise9/src/model/model.php
@@ -0,0 +1,14 @@
+<?php
+namespace model;
+
+class Operations {
+    public function concatenate($string1, $string2){
+        return $string1 . $string2;
+    }
+    public function split($result, $index){
+        return substr($result, $index, strlen($result) - $index);
+    }
+    public function replace($remove, $replace, $root){
+        return str_replace($remove, $replace, $root);
+    }
+}
