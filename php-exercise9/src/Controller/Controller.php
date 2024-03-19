6d6471e2c2e727f98104d0e3502d357a9aad055f
Peter Madrid <petermadrid0421@gmail.com>
2024-03-19 10:15:59 +0800

    modified code structure according to PST standards

--- php-exercise9/src/Controller/Controller.php
+++ php-exercise9/src/Controller/Controller.php
@@ -3,38 +3,40 @@ namespace src\Controller;
 
 use src\Model\OperationsModel;
 
-class Controller {
-    public function index(){
+class Controller
+{
+    public function index()
+    {
         $result = "";
         $result1 = "";
         $resilt2 = "";
         $x = 0;
         session_start();
 
-        if($_SERVER["REQUEST_METHOD"] == "POST"){
+        if ($_SERVER["REQUEST_METHOD"] == "POST") {
             $operation = new OperationsModel();
             $oper = $_POST["task"];
-            
-            if(isset($oper)){
-                switch($oper){
-                    case 'merge' :
+
+            if (isset($oper)) {
+                switch ($oper) {
+                    case 'merge':
                         $string1 = $_POST["first1"];
                         $string2 = $_POST["second1"];
-                        $_SESSION["result"] = $operation->concatenate($string1, $string2); 
+                        $_SESSION["result"] = $operation->concatenate($string1, $string2);
                         $result = $_SESSION["result"];
                         break;
-                    case 'split' :
+                    case 'split':
                         $index = $_POST["index"];
-                        if (isset($_SESSION["result"])) { 
-                            $x = $x+1;
-                            $result = $_SESSION["result"]; 
+                        if (isset($_SESSION["result"])) {
+                            $x = $x + 1;
+                            $result = $_SESSION["result"];
                             $_SESSION["result1"] = $operation->split($result, $index);
                             $result1 = $_SESSION["result1"];
                         }
                         break;
-                    case 'replace' :
+                    case 'replace':
                         $replace = $_POST['replace'];
-                        if (isset($_SESSION["result1"])){
+                        if (isset($_SESSION["result1"])) {
                             $x = 3;
                             $root = $_SESSION["result"];
                             $substringToRemove = $_SESSION["result1"];
@@ -47,6 +49,6 @@ class Controller {
         $result = $result ?? "";
         $result1 = $result1 ?? "";
         $resulit2 = $result2 ?? "";
-        require __DIR__ ."/../View/View.php";
+        require __DIR__ . "/../View/View.php";
     }
 }
--- php-exercise9/src/Model/OperationsModel.php
+++ php-exercise9/src/Model/OperationsModel.php
@@ -1,14 +1,18 @@
 <?php
 namespace src\Model;
 
-class OperationsModel {
-    public function concatenate($string1, $string2){
+class OperationsModel
+{
+    public function concatenate($string1, $string2)
+    {
         return $string1 . $string2;
     }
-    public function split($result, $index){
+    public function split($result, $index)
+    {
         return substr($result, $index, strlen($result) - $index);
     }
-    public function replace($remove, $replace, $root){
+    public function replace($remove, $replace, $root)
+    {
         return str_replace($remove, $replace, $root);
     }
 }
