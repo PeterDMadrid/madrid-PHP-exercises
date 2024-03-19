ea87914275e0c0bf16a283998a7f2e8366c0552c
Peter Madrid <petermadrid0421@gmail.com>
2024-03-19 10:10:36 +0800

    modified code structure according to PST standards

--- php-exercise7/src/Controller/Controller.php
+++ php-exercise7/src/Controller/Controller.php
@@ -3,31 +3,33 @@ namespace src\Controller;
 
 use src\Model\Model;
 
-class Controller {
-    public function index(){
+class Controller
+{
+    public function index()
+    {
         $first = '';
-$last = '';
-$em = '';
-$text = '';
+        $last = '';
+        $em = '';
+        $text = '';
 
-if($_SERVER["REQUEST_METHOD"] == "POST"){
-    $firstName = $_POST["first"];
-    $lastName = $_POST["last"];
-    $email = $_POST["email"];
-    $message = $_POST["message"];
+        if ($_SERVER["REQUEST_METHOD"] == "POST") {
+            $firstName = $_POST["first"];
+            $lastName = $_POST["last"];
+            $email = $_POST["email"];
+            $message = $_POST["message"];
 
-    $form = new Model($firstName, $lastName, $email, $message);
+            $form = new Model($firstName, $lastName, $email, $message);
 
-    $first = $form->getFirst();
-    $last = $form->getLast();
-    $em = $form->getEmail();
-    $text = $form->getText();
-}
+            $first = $form->getFirst();
+            $last = $form->getLast();
+            $em = $form->getEmail();
+            $text = $form->getText();
+        }
 
-$first = $first ?? "";
-$last = $last ?? "";
-$em = $em ?? "";
-$text = $text ?? "";
-require __DIR__ . "/../View/View.php";
+        $first = $first ?? "";
+        $last = $last ?? "";
+        $em = $em ?? "";
+        $text = $text ?? "";
+        require __DIR__ . "/../View/View.php";
     }
 }
--- php-exercise7/src/model/model.php
+++ php-exercise7/src/model/model.php
@@ -1,28 +1,34 @@
-<?php 
+<?php
 namespace src\Model;
 
-class Model {
+class Model
+{
     public $first;
     public $last;
     public $email;
     public $message;
-    public function __construct($first, $last, $email, $message){
+    public function __construct($first, $last, $email, $message)
+    {
         $this->first = $first;
         $this->last = $last;
         $this->email = $email;
         $this->message = $message;
     }
 
-    public function getFirst(){
+    public function getFirst()
+    {
         return $this->first;
     }
-    public function getLast(){
+    public function getLast()
+    {
         return $this->last;
     }
-    public function getEmail(){
+    public function getEmail()
+    {
         return $this->email;
     }
-    public function getText(){
+    public function getText()
+    {
         return $this->message;
     }
 }
